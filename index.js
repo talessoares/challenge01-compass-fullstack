fb3749685e3e3da73ea13f9b5c6e16f32eb5f121
Tales Schifelbein Soares <talesschifelbeinsoares@gmail.com>
2024-08-14 18:21:31 -0300

    feat: :sparkles: Adicionada a validação com JS
    Validação para os campos de email para o subscribe e para o forms (contact page)
    
    #27

---
+++ index.js
@@ -0,0 +1,65 @@
+document.getElementById("submitButton").addEventListener("click", function () {
+  const email = document.getElementById("emailInput").value;
+
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  if (emailRegex.test(email)) {
+    alert("Sucesso! Email " + email + " cadastrado na newsletter.");
+  } else if (email === "") {
+    alert("Erro! Campo de email vazio.");
+  } else {
+    alert("Erro! Email " + email + " inválido.");
+  }
+
+  document.getElementById("emailInput").value = "";
+});
+
+document
+  .querySelector(".contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const firstName = document.getElementById("firstName");
+    const lastName = document.getElementById("lastName");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!firstName.value.trim()) {
+      alert("Nome é obrigatório.");
+      firstName.focus();
+      return;
+    }
+
+    if (!lastName.value.trim()) {
+      alert("Sobrenome é obrigatório.");
+      lastName.focus();
+      return;
+    }
+
+    if (!email.value.trim()) {
+      alert("Email é obrigatório.");
+      email.focus();
+      return;
+    }
+
+    if (!emailRegex.test(email.value)) {
+      alert("Por favor, insira um email válido.");
+      email.focus();
+      return;
+    }
+
+    if (!message.value.trim()) {
+      alert("A mensagem é obrigatória.");
+      message.focus();
+      return;
+    }
+
+    alert("Obrigado! " + firstName.value + " formulário enviado com sucesso!");
+
+    firstName.value = "";
+    lastName.value = "";
+    email.value = "";
+    message.value = "";
+  });
